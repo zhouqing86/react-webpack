24a68169e4d2f95ab18dadd198f2b4f3f576c1ec
Qing Zhou <27952278@qq.com>
2022-08-06 20:30:11 +0800

    init react project with webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  mode: isDevelopment ? "development" : "production",
+  output: {
+    path:path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+}
